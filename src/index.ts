db03685abfc86791c5e3ec339fae23bbe534cf0e
airum82 <airum82@gmail.com>
2021-01-16 10:42:22 -0700

    Create copy and change to base setup

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,28 @@
 import * as Phaser from 'phaser';
 
-const config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+const config: Phaser.Types.Core.GameConfig = {
+	type: Phaser.AUTO,
+	width: 800,
+	height: 600,
+	scene: {
+			preload: preload,
+			create: create,
+			update: update
+	},
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 300 },
+			debug: true,
+		}
+	}
 };
 
 const game = new Phaser.Game(config);
 
 function preload ()
 {
+	
 }
 
 function create ()
@@ -23,4 +31,5 @@ function create ()
 
 function update ()
 {
+
 }
